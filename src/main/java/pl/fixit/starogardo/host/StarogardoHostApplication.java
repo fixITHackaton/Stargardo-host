e966b129b7acf452531dc9c8f200eeaed032b09b
Krzysztof Sajkowski <ksajkowski@mcx.pl>
2020-11-20 22:16:32 +0100

    Initial commit

---
+++ src/main/java/pl/fixit/starogardo/host/StarogardoHostApplication.java
@@ -0,0 +1,13 @@
+package pl.fixit.starogardo.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarogardoHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarogardoHostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/fixit/starogardo/host/StarogardoHostApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fixit.starogardo.host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarogardoHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
